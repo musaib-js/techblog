398f0179444dbc57411570b54c378cb6b4d7c77d
musaib-js <musaibaltafurahman@gmail.com>
2022-11-27 12:15:10 +0530

    initial commit

---
+++ iwtblog/home/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+CATEGORY_CHOICES = (
+    ('general', 'General'),
+    ('python', 'Python'),
+    ('javascript', 'JavaScript'),
+    ('C++', 'C++')
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    heading = models.CharField(max_length = 600)
+    author = models.CharField(max_length = 500)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(choices = CATEGORY_CHOICES, default="general", max_length=150)
+    description = models.TextField()
+    img = models.ImageField(upload_to='media')
+
+    def __str__(self):
+        return self.title
+    
